caef7e0a2dc3ac42dd3a9fd423fce3039b1803b2
Max Kearney <101630505@student.swin.edu.au>
2018-03-28 12:17:45 +1100

    Added item and report classes

---
+++ PharmaSave/PharmaSave/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaSave
+{
+    class Item
+    {
+        public int item_id;
+        public string item_name;
+        public float item_price;
+
+        Item()
+        {
+            item_id = 0;
+            item_name = "";
+            item_price = 0.0f;
+        }
+
+        string GetCSValues
+        {
+            get
+            {
+                return item_id.ToString() + "," + item_name + "," + item_price.ToString();
+            }
+        }
+    }
+}
---
+++ PharmaSave/PharmaSave/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaSave
+{
+    class Report
+    {
+        private Dictionary<Item, int> items;
+
+        Report()
+        {
+            items = new Dictionary<Item, int>();
+        }
+
+        void AddItem(Item item, int amount)
+        {
+            items.Add(item, amount);
+        }
+
+        Dictionary<Item, int> GetItems
+        {
+            get
+            {
+                return items;
+            }
+        }
+
+    }
+}
