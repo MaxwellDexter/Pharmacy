0729bbeaa2762fab34e2862ccbd1c7c11847902d
Hayley-K <101098200@student.swin.edu.au>
2018-04-04 22:47:58 +1000

    started homepage form

--- PharmaSave/PharmaSave/Form1.cs
+++ PharmaSave/PharmaSave/Form1.cs
@@ -16,5 +16,20 @@ namespace PharmaSave
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
